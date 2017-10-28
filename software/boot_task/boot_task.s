ddc62d9d57660d44d05268e81e24d03e5cf2c55b
Marcos Sartori <msartori@thesiriusproject.com>
2017-10-28 19:10:37 -0200

    Fixed boot_task.asm compilation

--- software/boot_task/boot_task.s
+++ software/boot_task/boot_task.s
@@ -1,12 +1,12 @@
-## HEMPS VERSION - 8.0 - support for RT applications
-##
-## Distribution:  June 2016
-##
-## Created by: Marcelo Ruaro - contact: marcelo.ruaro@acad.pucrs.br
-##
-## Research group: GAPH-PUCRS   -  contact:  fernando.moraes@pucrs.br
-##
-## Brief description: Initializes the stack pointer and jumps to main(). Handles the syscall.
+# HEMPS VERSION - 8.0 - support for RT applications
+#
+# Distribution:  June 2016
+#
+# Created by: Marcelo Ruaro - contact: marcelo.ruaro@acad.pucrs.br
+#
+# Research group: GAPH-PUCRS   -  contact:  fernando.moraes@pucrs.br
+#
+# Brief description: Initializes the stack pointer and jumps to main(). Handles the syscall.
 
 
 
@@ -17,53 +17,52 @@
 entry:
   .set noreorder
 
-  li $sp,sp_addr ; new initialization
+  la $sp,sp_addr # new initialization
 
   jal  main
   nop
 	
-	beq $zero, $zero, exit
-	or  $a0, $v0, $zero
+	beq $0, $0, exit
+	or  $4, $2, $0
 	.end entry
 
 	.globl exit
 	.ent exit
 	.set noreorder
 exit:
-	lui $s0, 0x2000								; $s0 = *HARDWARE_REGISTERS
-	la  $s1, .RET_MSG							; $s1 = *RET_MSG
+	lui $16, 0x2000								# $16 = *HARDWARE_REGISTERS
+	la  $17, .RET_MSG							# $17 = *RET_MSG
 
-	;; assemble the package
-	lw  $t0, 320($s0) 						; $t0 = NET_ADDRESS
-	lw  $t1, 324($s0) 						; $t1 = LOADER_NETADDR
-	sw  $t0, 12($s1)							; RET_MSG.SOURCE = $t0
-	sw  $t1, 0($s1)								; RET_MSG.TARGET = $t1
-	ori $t0, $zero, 5							; $t0 = 5 (DMNI_SIZE)
-	sw  $a0, 0($s1)								; RET_MSG.RETURN_CODE = $a0
+	# assemble the package
+	lw  $8, 320($16) 						# $8 = NET_ADDRESS
+	lw  $9, 324($16) 						# $9 = LOADER_NETADDR
+	sw  $8, 12($17)							# RET_MSG.SOURCE = $8
+	sw  $9, 0($17)							# RET_MSG.TARGET = $9
+	ori $8, $0, 5								# $8 = 5 (DMNI_SIZE)
+	sw  $4, 0($17)							# RET_MSG.RETURN_CODE = $4
 
-	;; send it
-	sw	$t0, 512($s0)							; *DMNI_SIZE = $t0
-	sw  $s1, 528($s0)							; *DMNI_ADDR = $s1
-	ori $t0, $zero, 1 						; $t0 = 1
-	sw  $zero, 544($s0)						; READ -> DMNI_OP
-	sw  $t0, 560($s0) 						; 1 -> DMNI_START
+	# send it
+	sw	$8, 512($16)						# *DMNI_SIZE = $8
+	sw  $17, 528($16)						# *DMNI_ADDR = $17
+	ori $8, $0, 1 							# $8 = 1
+	sw  $0, 544($16)						# READ -> DMNI_OP
+	sw  $8, 560($16) 						# 1 -> DMNI_START
 
-	;; commit suicide
-	li $t0, 0xDEADBEAF 						; MAGIC WORD TO KILL CPU
+	# commit suicide
+	li $8, 0xDEADBEAF 					# MAGIC WORD TO KILL CPU
 .L1:
-  beq $zero, $zero, .L1	
-  sw $t0, 800($s0) 							; SET_CPU_KILL
+  beq $0, $0, .L1	
+  sw $8, 800($16) 						# SET_CPU_KILL
 	.end exit
 
-; memory region reserved to assemble termination pkg
+# memory region reserved to assemble termination pkg
 .RET_MSG:
-	.word 0 											; TARGET
-	.word 2												; SIZE
-	.word 0x70										; SERVICE
-	.word 0												; SOURCE
-	.word 0												; RETURN_CODE
+	.word 0 											# TARGET
+	.word 2												# SIZE
+	.word 0x70										# SERVICE
+	.word 0												# SOURCE
+	.word 0												# RETURN_CODE
   
-###################################################
 
    .globl SystemCall
    .ent SystemCall
