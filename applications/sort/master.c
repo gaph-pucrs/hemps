5c2b8be7375d8c7548e59b2b7920ca9080f0038f
Alzemiro Lucas Silva (Aluno MSc - Moraes) <alzemiro@gmail.com>
2017-11-17 09:19:58 -0200

    master slave quicksort application added

---
+++ applications/sort/master.c
@@ -0,0 +1,80 @@
+#include <libos.h>
+#include "map_pkg.h"
+
+#include "sort.h"
+
+#define TASKS 40
+#define SLAVES 3
+
+void init_array(int *array, int size){
+	int i;
+
+	for (i = 0; i < size; i++)
+		array[i] = size - i;
+}
+
+void print_array(int *array, int size){
+	int i;
+	for (i = 0; i < size; i++) {
+		puts(itoasc(array[i]));
+		puts(" ");
+	}
+	puts("\n");
+
+}
+
+int get_id(int src) {
+	if (src==slave1)
+		return 0;
+	if (src==slave2)
+		return 1;
+	if (src==slave3)
+		return 2;	
+}
+
+int main(){
+	int i;
+	int *buff;
+	int slave_addr[SLAVES] = {slave1, slave2, slave3};
+
+	int array[TASKS][ARRAY_SIZE];
+	int slave_task[SLAVES];
+	int task = 0;
+	int slave_id;
+
+	int msg_kill;
+	flit_t src;
+	size_t size;
+
+	for (i = 0; i < TASKS; i++)
+		init_array(array[i], ARRAY_SIZE);
+
+	for (i = 0; i < SLAVES; i++){
+		prepare_recv_msg(&src, &size);
+		printf("Waiting to receive %d bytes from slave %X\n", size, src);
+		buff = wait_receive();
+		puts(itoasc(buff[0]));
+		puts("\n");
+		slave_task[buff[0]] = task;
+		send_msg(slave_addr[buff[0]], (unsigned int*)array[task], ARRAY_SIZE*4);
+		task++;
+	}
+
+	for (i = 0; i < TASKS; i++){
+		prepare_recv_msg(&src, &size);
+		printf("Waiting to receive %d bytes from slave %X\n", size, src);
+		buff = wait_receive();
+		print_array(buff, ARRAY_SIZE);
+		slave_id = get_id(src);
+		slave_task[slave_id] = task;
+		if (task == TASKS){
+			send_msg(slave_addr[slave_id], &msg_kill, 4);
+			printf("Master Sening kill to %X\n", src);
+		}
+		else {
+			send_msg(slave_addr[slave_id], (unsigned int*)array[task], ARRAY_SIZE*4);
+			task++;
+		}
+	}
+		
+}
---
+++ applications/sort/sort.h
@@ -0,0 +1,80 @@
+#include <libos.h>
+
+#define ARRAY_SIZE 40
+#define LFSR_POL 0xA3000000
+
+#define TASK_REQUEST    1
+#define KILL_PROC		2
+#define MSG_ACK			3
+
+char *itoasc(unsigned int num)
+{
+   static char buf[12];
+   static char buf2[12];
+   int i,j;
+
+   if (num==0)
+   {
+      buf[0] = '0';
+      buf[1] = '\0';
+      return &buf[0];
+   }
+
+   for(i=0;i<11 && num!=0;i++)
+   {
+      buf[i]=(char)((num%10)+'0');
+      num/=10;
+   }
+   buf2[i] = '\0';
+   j = 0;
+   i--;
+   for(;i>=0;i--){
+         buf2[i]=buf[j];
+         j++;
+   }
+   return &buf2[0];
+}
+
+unsigned int rand_val = LFSR_POL;
+
+unsigned int rand(){
+	rand_val = (rand_val >> 1) ^ (-(rand_val & 1u) & LFSR_POL);
+	return rand_val;
+}
+
+void swap(int *array, int a, int b){
+	int aux;
+
+	aux = array[a];
+	array[a] = array[b];
+	array[b] = aux;
+}
+
+int partition(int *array, int lo, int hi){
+	int pivo, i, j;
+	int index;
+
+	i = lo - 1;
+	index = rand() % (hi - lo) + lo;
+	swap(array, index, hi);
+	pivo = array[hi];
+
+	for (j = lo; j < hi; j++){
+		if (array[j] < pivo){
+			i++;
+			swap(array, i, j);
+		}
+	}
+	if (array[hi] < array[i+1])
+		swap(array, i+1, hi);
+	return i+1;
+}
+
+void quicksort(int *array, int lo, int hi) {
+	int p;
+    if (lo < hi){
+        p = partition(array, lo, hi);
+        quicksort(array, lo, p - 1);
+        quicksort(array, p + 1, hi);
+    }
+}
