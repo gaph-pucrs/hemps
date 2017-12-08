c330726b616be0725072dbaa1f53e27e7a6ce89d
Luis Fernando Krause (Academico - Hessel) <krause@gaphl10.(none)>
2017-12-08 11:51:29 -0200

    testbench rodando aplicacoes em paralelo

--- hardware/vhdl/test_bench.vhd
+++ hardware/vhdl/test_bench.vhd
@@ -23,6 +23,14 @@ use work.standards.all;
 
 entity test_bench is
 
+  type procload is record
+    position  : std_logic_vector(31 downto 0);
+    status : std_logic;
+  end record;
+
+  type list_of_procs is array(0 to MAX_TASKS_APP-1) of procload;
+
+
   type config is record
     code_name : string(1 to 40);
     position  : std_logic_vector(31 downto 0);
@@ -91,6 +99,38 @@ entity test_bench is
     return cfg;
   end get_cfg;
 
+
+  impure function get_procs (cfg_file : in string) return list_of_procs is
+
+    file file_ptr    : text open read_mode is cfg_file;
+    variable inline  : line;
+    variable strline : string(1 to 40);
+    variable procs     : list_of_procs;
+    variable cfg     : list_of_tasks;
+    variable str_len : integer;
+    variable i       : integer := 0;
+  begin
+
+   while not endfile(file_ptr) loop
+      readline(file_ptr, inline);
+      str_len := inline'length;
+      read(inline, strline(1 to inline'length));
+      cfg(i).code_name(1 to str_len) := strline(1 to str_len);
+      cfg(i).code_name(str_len+1 to 40) := (others => NUL);
+      readline(file_ptr, inline);
+      hread(inline, cfg(i).position);
+      procs(i).position := cfg(i).position;
+      procs(i).status := '1';
+      readline(file_ptr, inline);
+      hread(inline, cfg(i).code_size);
+      i := i + 1;
+    end loop;
+    file_close(file_ptr);
+    return procs;
+  end get_procs;
+
+
+
   impure function get_num_tasks(cfg_file : in string) return integer is
 
     file file_ptr    : text open read_mode is cfg_file;
@@ -128,7 +168,33 @@ entity test_bench is
     return apps_cfg;
   end get_apps_cfg;
 
+
+  impure function next_wave (taloc : in integer; vapp_counter: in integer; vapps_cfg: list_of_apps;  vlisttask_ocupacao: arrayNPe_reg32) return integer is
+     variable talocret: integer := 0;
+      variable task_cfg          : list_of_tasks;
+      variable proc_load         : list_of_procs;
+      variable task_num          : integer;    
+  begin  
+   if (vapp_counter+1 < APP_NUMBER) then
+          task_cfg      := get_cfg(vapps_cfg(vapp_counter+1));
+          proc_load     := get_procs(vapps_cfg(vapp_counter+1));
+          task_num      := get_num_tasks(vapps_cfg(vapp_counter+1));  
+       for i in 0 to task_num-1 loop
+              for j in 0 to NUMBER_PROCESSORS-1 loop 
+                  if (task_cfg(i).position = RouterAddress(j)) then
+                      if (vlisttask_ocupacao(j) = x"00000001") then
+                          talocret := 1;
+                      end if;
+                  end if;
+              end loop; 
+      end loop;
+   end if;   
+
+ return talocret; 
+  end next_wave;
+
 end;
+
 
 architecture test_bench of test_bench is
 
@@ -152,8 +218,12 @@ architecture test_bench of test_bench is
   signal clock_rx_io : std_logic_vector((IO_NUMBER-1) downto 0);
 
   --Leitura para IO
+  
   signal task_cfg          : list_of_tasks;
+  signal ntask_cfg          : list_of_tasks;
+  signal proc_load         : list_of_procs;
   signal task_num          : integer;
+  signal ntask_num          : integer;
   signal rd_addr           : std_logic_vector(23 downto 0);
   signal flit_counter      : integer;
   signal file_counter      : integer;
@@ -163,6 +233,13 @@ architecture test_bench of test_bench is
   signal packstart_data    : packstart_type;
   signal task_code         : repo_type;
 
+ --- Variaveis novas testbench
+  signal listtask_ocupacao_ret  : arrayNPe_reg32:= (others=>x"00000010");
+  signal listtask_ocupacao_env  : arrayNPe_reg32:= (others=>x"00000010");
+  signal listtask_ocupacao  : arrayNPe_reg32;
+  
+   --- Variaveis novas testbench Fim 
+
   type send_state is (WAIT_state, GET_CODE, SEND_TASK, SEND_START);
   signal SEND: send_state;
 
@@ -171,7 +248,7 @@ begin
   packstart_data(0) <= x"00000002";
   packstart_data(1) <= x"00000300";
   packstart_data(2)  <= x"00000000";
-
+  
   --
   --  HeMPS instantiation 
   --
@@ -230,6 +307,37 @@ begin
       );
 
 
+
+---------------------------------------------------------------------------------------------
+-- recebe retornos de encerramento
+---------------------------------------------------------------------------------------------
+
+  process
+  begin
+    wait until (clock_tx_io(0)'event and clock_tx_io(0) = '1') or (reset'event and reset = '1');
+    if reset = '1' then
+        for i in 0 to NUMBER_PROCESSORS -1 loop
+            listtask_ocupacao_ret(i) <=  listtask_ocupacao_ret(i) + x"00000000";
+        end loop;  
+    else
+    	if (data_out_io(0) = x"E0000000") then
+         wait until (clock_tx_io(0)'event and clock_tx_io(0) = '1');
+         wait until (clock_tx_io(0)'event and clock_tx_io(0) = '1');
+         wait until (clock_tx_io(0)'event and clock_tx_io(0) = '1');
+         if (data_out_io(0) = x"00000070") then
+            wait until (clock_tx_io(0)'event and clock_tx_io(0) = '1');
+            for i in 0 to NUMBER_PROCESSORS -1 loop
+              if (RouterAddress(i) = data_out_io(0)) then
+                listtask_ocupacao_ret(i) <=  listtask_ocupacao_ret(i) + x"00000001";
+              end if;
+            end loop;  
+         end if;
+    	end if;
+    end if;
+  end process;
+
+
+
   reset     <= '1', '0'      after 100 ns;
   -- 100 MHz
   clock     <= not clock     after 5 ns;
@@ -238,27 +346,61 @@ begin
 
   --Conecta o testbench inicialmente no IO 0
   clock_rx_io(0) <= clock;
-  process (clock, reset)
+  process
+    variable  liberar_envio              : integer := 1;
   begin
+    wait until (clock'event and clock = '1') or (reset'event and reset = '1');
     if reset = '1' then
       rx_io(0)          <= '0';
       rd_addr           <= (others => '0');
-      flit_counter      <= 0;
+      flit_counter      <= 0; 
       packstart_counter <= 0;
       file_counter      <= 0;
       app_counter       <= 0;
       task_num          <= 0;
+      liberar_envio             := 1;
       SEND              <= WAIT_state;
-    elsif rising_edge(clock) then
+    else 
+      for i in 0 to NUMBER_PROCESSORS-1 loop
+        listtask_ocupacao(i) <= listtask_ocupacao_env(i) - listtask_ocupacao_ret(i);
+      end loop;
       case SEND is
         when WAIT_state =>
-          if app_counter < APP_NUMBER then
+        if (liberar_envio = 0) then
+             liberar_envio := 1; 
+             for i in 0 to ntask_num-1 loop
+                    for j in 0 to NUMBER_PROCESSORS-1 loop 
+                        if (ntask_cfg(i).position = RouterAddress(j)) then
+                            if (listtask_ocupacao(j) = x"00000001") then
+                                liberar_envio := 0;
+                            end if;
+                        end if;
+                    end loop; 
+             end loop;        
+        end if;
+        wait until (clock'event and clock = '1');
+          if ((app_counter < APP_NUMBER) and (liberar_envio = 1)) then
+            liberar_envio    := 0;
             task_cfg      <= get_cfg(apps_cfg(app_counter));
-            task_num      <= get_num_tasks(apps_cfg(app_counter));
+            proc_load     <= get_procs(apps_cfg(app_counter));
+            task_num      <= get_num_tasks(apps_cfg(app_counter)); 
+            if(app_counter+1 < APP_NUMBER) then
+                ntask_cfg      <= get_cfg(apps_cfg(app_counter+1));
+                ntask_num      <= get_num_tasks(apps_cfg(app_counter+1));
+            end if;
+            wait until (clock'event and clock = '1');
+            for i in 0 to NUMBER_PROCESSORS-1 loop
+              for j in 0 to task_num-1 loop
+                if (proc_load(j).position = RouterAddress(i)) then
+                  listtask_ocupacao_env(i) <= listtask_ocupacao_env(i) + x"00000001";
+                end if;
+              end loop; 
+            end loop;        
             SEND          <= GET_CODE;
+
           end if;
 
-        when GET_CODE =>
+        when GET_CODE =>  
           if file_counter < task_num then
             flit_counter  <= CONV_INTEGER(task_cfg(file_counter).code_size);
             task_code     <= load_repo(task_cfg(file_counter).code_name);
@@ -267,10 +409,10 @@ begin
           else
             app_counter   <= app_counter + 1;
             file_counter  <= 0;
-            SEND          <= WAIT_state;
+            SEND          <= WAIT_state; 
           end if;
 
-        when SEND_TASK => 
+        when SEND_TASK =>  
           if credit_o_io(0) = '1' and flit_counter > 0 then
             rx_io(0) <= '1';
             rd_addr  <= rd_addr + 1;
@@ -285,13 +427,13 @@ begin
               flit_counter  <= flit_counter - 1;
             end if;
           elsif flit_counter = 0 then
-            SEND      <= SEND_START;
+            SEND      <= SEND_START; 
             rd_addr   <= (others => '0');
             rx_io(0) <= '0';
           end if;
 
         when SEND_START =>
-          if credit_o_io(0) = '1' and  rd_addr < TAM_PACKSTART then
+          if credit_o_io(0) = '1' and  rd_addr < TAM_PACKSTART then 
             rx_io(0) <= '1';
             rd_addr  <= rd_addr + 1;
             if rd_addr = 0 then
